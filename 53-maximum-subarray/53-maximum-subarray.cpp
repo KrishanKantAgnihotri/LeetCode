1df3aeaaa570d5e78d3958205bc9891d08f1989f
Krishan Kant Agnihotri <60218702+KrishanKantAgnihotri@users.noreply.github.com>
2022-06-26 18:11:03 +0530

    Time: 190 ms (27.04%), Space: 67.7 MB (53.39%) - LeetHub

---
+++ 53-maximum-subarray/53-maximum-subarray.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    int maxSubArray(vector<int>& v) {
+        int ans = *max_element(v.begin(),v.end());
+        int cur = 0 ; 
+        int sm = 0 ; 
+        for(int i = 0 ;i<v.size() ;i++){
+            cur+=v[i];
+            if(cur<v[i])
+                cur = v[i];
+            // cout<<cur<<" ";
+            ans = max(ans,cur);
+        }
+        return ans;
+    }
+};
