769c7ed8834a885f24b09833808e51e57f1fd990
Krishan Kant Agnihotri <60218702+KrishanKantAgnihotri@users.noreply.github.com>
2022-08-22 10:35:58 +0530

    Time: 2 ms (60.92%), Space: 5.9 MB (71.30%) - LeetHub

--- 342-power-of-four/342-power-of-four.cpp
+++ 342-power-of-four/342-power-of-four.cpp
@@ -2,8 +2,11 @@ class Solution {
 public:
     bool isPowerOfFour(int n) {
         
-        if(__builtin_popcount(n) == 1){
-            return (ffs(n)%2 == 1);
+        if(__builtin_popcountll(n)!=1) return false;
+        for(int i = 0 ;i<32 ;i++){
+            if((1<<i)&n){
+                return (i%2 == 0);
+            }
         }
         return false;
     }
