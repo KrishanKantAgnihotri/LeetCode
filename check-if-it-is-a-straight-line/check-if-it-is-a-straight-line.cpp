0b56c7ec753dc422d85d90296b6b6d88f9f54ddc
Krishan Kant Agnihotri <60218702+KrishanKantAgnihotri@users.noreply.github.com>
2023-06-05 12:08:09 +0530

    Time: 17 ms (6.34%) | Memory: 10.4 MB (7.38%) - LeetSync

---
+++ check-if-it-is-a-straight-line/check-if-it-is-a-straight-line.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    bool checkStraightLine(vector<vector<int>>& v) {
+        vector<pair<int,int>> points;
+        for(int i = 0 ;i<v.size() ;i++){
+            points.push_back({v[i][0],v[i][1]});
+        }
+        sort(points.begin(),points.end());
+        int n = points.size();
+        if(points.size()==2){
+            return true;
+        }
+        int y = points[1].second-points[0].second;
+        int x = points[1].first -points[0].first;
+        for(int i = 2 ;i<n ;i++){
+            int y1 = points[i].second-points[i-1].second;
+            int x1 = points[i].first-points[i-1].first;
+            if(y*x1 != y1*x){
+                return false;
+            }
+        }
+        return true;
+        
+    }
+};
