067f508e1bcc0c120c24b1c1aa33f3b0b46b4033
Krishan Kant Agnihotri <60218702+KrishanKantAgnihotri@users.noreply.github.com>
2023-07-07 12:08:03 +0530

    Time: 75 ms (20.79%) | Memory: 10.2 MB (33.14%) - LeetSync

--- maximize-the-confusion-of-an-exam/maximize-the-confusion-of-an-exam.cpp
+++ maximize-the-confusion-of-an-exam/maximize-the-confusion-of-an-exam.cpp
@@ -1,38 +1,18 @@
 class Solution {
 public:
     int maxConsecutiveAnswers(string s, int k) {
-        queue<int> q;
-        int ans = 0 ; 
         int n = s.length();
-        int cnt = 0 ;
-        bool ok = false; 
-        // for making things for 'F'
+        map<char,int> m;
+        int ans =  0 ; 
         for(int i = 0 ;i<n ;i++){
-            cnt++;
-            if(s[i] == 'T'){
-                q.push(i);
+            m[s[i]]++;
+            int cnt = min(m['T'],m['F']);
+            if(cnt<=k){
+                ans++;
             }
-            if(q.size()>k){
-                cnt = i-q.front();
-                q.pop();
+            else{
+                m[s[i-ans]]--;
             }
-            ans = max(ans,cnt);
-        }
-        cnt =  0;
-        while(!q.empty()){
-            q.pop();
-        }
-         ok = false;
-        for(int i =  0 ;i<n; i++){
-            cnt++;
-            if(s[i]  == 'F'){
-                q.push(i);
-            }
-            if(q.size()>k){
-                cnt = i-q.front();
-                q.pop();
-            }
-            ans = max(ans,cnt);
         }
         return ans;
     }
