a676d32cfee5568d03c3d571d26d85cebf57b261
Krishan Kant Agnihotri <60218702+KrishanKantAgnihotri@users.noreply.github.com>
2023-08-17 17:36:31 +0530

    Time: 6 ms (45.22%) | Memory: 9.7 MB (31.66%) - LeetSync

--- 990-verifying-an-alien-dictionary/verifying-an-alien-dictionary.cpp
+++ 990-verifying-an-alien-dictionary/verifying-an-alien-dictionary.cpp
@@ -2,25 +2,21 @@ class Solution {
 public:
     bool isAlienSorted(vector<string>& words, string order) {
        unordered_map<char,char> u;
-       unordered_map<char,char> r;
-       vector<string> new_words;
+      
+       
        for(int i = 0 ;i<order.length() ;i++){
            u[order[i]] = char(i+'a');
-           r[char(i+'a')] = order[i];
+           
        } 
        for(int i = 0 ;i<words.size() ;i++){
            string s = words[i];
            for(int j = 0 ;j<s.length();j++){
                s[j] = u[s[j]];
            }
-           new_words.push_back(s);
+           words[i] = s;
+           
        }
-       sort(new_words.begin(),new_words.end());
-       for(int i = 0 ;i<new_words.size();i++){
-           for(int j = 0 ;j<new_words[i].length() ;j++){
-               new_words[i][j] = r[new_words[i][j]];
-           }
-       }
-       return new_words == words;
+      
+       return is_sorted(words.begin(),words.end());
     }
 };
