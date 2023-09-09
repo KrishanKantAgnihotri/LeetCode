c42ebbbcac0bbcccd260078865ff532f2a737cd9
Krishan Kant Agnihotri <60218702+KrishanKantAgnihotri@users.noreply.github.com>
2023-09-09 11:40:36 +0530

    Time: 0 ms (100.00%) | Memory: 6.7 MB (46.24%) - LeetSync

---
+++ 377-combination-sum-iv/combination-sum-iv.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    int combinationSum4(vector<int>& nums, int target) {
+        vector<unsigned int> dp(target+1,0);
+        dp[0] = 1;
+        int n = nums.size();
+        sort(nums.begin(),nums.end());
+        for(int i = 1; i<=target ;i++){
+            for(int j = 0 ;j<n ;j++){
+                if(i>=nums[j])
+                dp[i]+=dp[i-nums[j]];
+            }
+        }
+        return dp[target];
+    }
+};
