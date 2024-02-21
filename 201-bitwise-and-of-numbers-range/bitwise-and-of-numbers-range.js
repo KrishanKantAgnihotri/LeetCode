535cd7749aad2c168bcd4e1860696e61631804b7
Krishan Kant Agnihotri <60218702+KrishanKantAgnihotri@users.noreply.github.com>
2024-02-21 10:26:15 +0530

    Time: 150 ms (14.15%) | Memory: 54.9 MB (48.78%) - LeetSync

---
+++ 201-bitwise-and-of-numbers-range/bitwise-and-of-numbers-range.js
@@ -0,0 +1,10 @@
+/**
+ * @param {number} left
+ * @param {number} right
+ * @return {number}
+ */
+var rangeBitwiseAnd = function(left, right) {
+    let cnt = 0 ; 
+    for(;left!=right;left>>=1,right>>=1,cnt++){}
+    return left<<cnt;
+};
