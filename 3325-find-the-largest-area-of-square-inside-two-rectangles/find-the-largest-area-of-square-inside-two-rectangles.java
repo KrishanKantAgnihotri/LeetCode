60ca31d9c10f3a16d795c537336ec5ee0b5527c3
Krishan Kant Agnihotri <60218702+KrishanKantAgnihotri@users.noreply.github.com>
2024-03-02 12:32:25 +0530

    Time: 77 ms (89.12%) | Memory: 44.7 MB (95.11%) - LeetSync

--- 3325-find-the-largest-area-of-square-inside-two-rectangles/find-the-largest-area-of-square-inside-two-rectangles.java
+++ 3325-find-the-largest-area-of-square-inside-two-rectangles/find-the-largest-area-of-square-inside-two-rectangles.java
@@ -1,33 +1,17 @@
 class Solution {
-    public static long getIntersectionArea(long l1,long l2,long r1,long r2){
-        // 4 cases for intersection 
-        // 1: when ith engulfs jth.
-        // 2: when jth engulfs ith.
-        // 3: when l2 lies b/w l1 and r1.
-        // 4:when l2 lies b/w l1 and r2.
-        if(l1<=l2 && r1>=r2){
-            return r2-l2;
-        } 
-        if(l2<=l1 && r1<=r2){
-            return r1-l1;
-        }
-        if(r1>=l2 && r2>=r1){
-            return r1-l2;
-        }
-        if(r1>=l2 && r2>=l1){
-            return r2-l1;
-        }
-        return 0 ; 
-    }
-    public long largestSquareArea(int[][] bLeft, int[][] tRight) {
+    public long largestSquareArea(int[][] bottomLeft, int[][] topRight) {
         long ans = 0 ; 
-        for(int i = 0 ;i<bLeft.length;i++){
-            for(int j = i+1 ;j<tRight.length;j++){
-                long x = getIntersectionArea(bLeft[i][0],bLeft[j][0],tRight[i][0],tRight[j][0]);//x intersection
-                long y = getIntersectionArea(bLeft[i][1],bLeft[j][1],tRight[i][1],tRight[j][1]);//y intersection
-                ans = Math.max(ans,Math.min(x,y));// square can be formed of side which is equal to minimum of 2 intersection
+        for(int i = 0 ;i<bottomLeft.length;i++){
+            for(int j = i+1 ;j<topRight.length;j++){
+                int minX = Math.min(topRight[i][0],topRight[j][0]);
+                int maxX = Math.max(bottomLeft[i][0],bottomLeft[j][0]);
+                int minY = Math.min(topRight[i][1],topRight[j][1]);
+                int maxY = Math.max(bottomLeft[i][1],bottomLeft[j][1]);
+                if(maxX<minX && maxY<minY){
+                    ans = Math.max(ans,Math.min(minX-maxX,minY-maxY));
+                }
             }
         }
-        return ans*ans;//area of that square
+        return ans*ans;
     }
 }
